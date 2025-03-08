38d6e8552e7f8b201b0a23bc0f95b82853f66404
EshanRavindu17 <eshanravindu17@gmail.com>
2025-03-08 09:10:00 +0530

    Create server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+// Import the express module
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Set the server to listen on port 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
