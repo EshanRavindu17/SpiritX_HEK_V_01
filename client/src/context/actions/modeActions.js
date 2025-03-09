46a9f970f8814c819fae969222dff82c8a7bb950
Krishmalgg <krishmalgamage20021027@gmail.com>
2025-03-09 18:58:39 +0530

    all

---
+++ client/src/context/actions/modeActions.js
@@ -0,0 +1,14 @@
+// context/actions/modeActions.js
+
+export const setMode = (mode) => ({
+    type: 'SET_MODE',
+    payload: mode,
+  });
+  
+  export const setProfileVisibility = (visibility) => ({
+    type: 'SET_PROFILE_VISIBILITY',
+    payload: visibility,
+  });
+  
+  
+  
